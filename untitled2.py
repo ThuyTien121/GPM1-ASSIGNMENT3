c6c002b23a16c32e56e34def291ab601e290d2fe
ThuyTien121 <161817350+ThuyTien121@users.noreply.github.com>
2025-03-13 18:20:26 +0700

    Update untitled2.py

--- untitled2.py
+++ untitled2.py
@@ -11,7 +11,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import plotly.express as apx
+import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from datetime import date, datetime
