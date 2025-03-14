a1c0c91b8338a6f385860285a7487a18513de2e6
ThuyTien121 <161817350+ThuyTien121@users.noreply.github.com>
2025-03-14 13:23:30 +0700

    Update untitled2.py

--- untitled2.py
+++ untitled2.py
@@ -32,10 +32,10 @@ COLOR_MAP = {
 }
 
 # File paths
-VOLUME_PATH = r"D:\Documents\Vietnam_volume_cleaned.csv"
-PRICE_PATH = r"D:\Documents\Vietnam_Price_cleaned.csv"
-SECTOR_PATH = r"D:\Documents\Phan_loai_nganh.csv"
-INVESTOR_DATA_PATH = r"D:\Documents\combined_data.csv"
+VOLUME_PATH = r'D:\Documents\Vietnam_volume_cleaned.csv'
+PRICE_PATH = r'D:\Documents\Vietnam_Price_cleaned.csv'
+SECTOR_PATH = r'D:\Documents\Phan_loai_nganh.csv'
+INVESTOR_DATA_PATH = r'D:\Documents\combined_data.csv'
 
 # Thiết lập trang
 st.set_page_config(page_title="Dashboard Giao dịch Toàn diện", layout="wide")
